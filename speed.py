b561360f4d7fa9a3659eca6878681710f9e477c0
Widmark-Kaue <widmarkkaue.s.c@gmail.com>
2025-04-01 15:40:01 -0300

    plot torque x velocidade com pontos para ótima troca de marcha

--- speed.py
+++ speed.py
@@ -10,17 +10,6 @@ import spl3
 import root
 import matplotlib.pylab as plt
 
-# Dados para Ferrari 360 Modena
-# Raio
-R = 0.3385 # metros
-
-# Razão das marchas 
-ratio = np.array([ 14.59,   9.58,   7.14,   5.63,   4.59,   3.77])
-
-# Tabela de Dados velocidade de rotaçao (rad/s) vs torque (Nm)
-w      = np.array([ 104.7,  157. ,  209.4,  261.8,  314.15,  366.5 ,  418.9 , 471.2 ,  523.6 ,  575.9 ,  628.3 ,  680.7 ,  733.  ,  785.4 , 837.7 ,  890.1 ,  942.5 ])
-torque = np.array([ 170.8,  256.2,  298.8,  324.5,   341.5,  353.7,   362.9,  370. ,   372.8 ,  371.3 ,  368.3 ,  363.7 ,  357.6 ,  350.  ,  340.9,  330.3 ,  280.7 ]) 
-
 def gearShift(torque, w, ratio, R):
     """
     Retorna um vetor com as velocidade ideiais para a troca de marcha.
@@ -40,6 +29,8 @@ def gearShift(torque, w, ratio, R):
     -------
     Trm : Array
         Vetor de velocidades ótimas para a troca de marcha.
+    TT: Array
+        Vetor de torques na velocidade ideal.
 
     """
     n1, = torque.shape
@@ -49,6 +40,7 @@ def gearShift(torque, w, ratio, R):
     T1 = np.zeros(n3) #vetor de funções do torque em relação a velocidade
     T2 = np.zeros(n3)
     Trm = np.zeros (n3 - 1) #vetor com velocidades ideiais para troca de marcha
+    TT = np.zeros (n3 - 1) # vetor com torque na velocidade ideal
     v1 = (w*R)/ratio[0]
     a1, b1, c1, d1 = spl3.spl3Coeff(v1,torque)
     for i in range(n3-1):
@@ -68,12 +60,13 @@ def gearShift(torque, w, ratio, R):
             else:
                 v = np. v = np.linspace(v1[0], v1[-1],20)
         n, trm = root.bissect(T,v[0],v[-1])
+        TT[i] = T1(trm)
         Trm[i] = trm
         v1 = v2
         a1, b1, c1, d1 = a, b, c, d
-    return Trm
+    return Trm, TT
 
-def plot (torque, w, ratio, R):
+def plot (torque, w, ratio, R, gearShift = None):
     """
     Plotar um gráfico com as curvas de torque para cada marcha.
 
@@ -86,13 +79,14 @@ def plot (torque, w, ratio, R):
     ratio : Array
         Razão entre as marchas.
     R : Float
-        Raio....
+        Raio.
 
     Returns
     -------
     None.
 
     """
+    plt.figure()
     n, = ratio.shape
     Wid = np.array(["1º marcha", "2º marcha","3º marcha","4º marcha","5º marcha","6º marcha"])
     for i in range (n):
@@ -103,12 +97,36 @@ def plot (torque, w, ratio, R):
         yg = np.zeros(N)
         for j in range (N):
             yg[j] = spl3.spl3EvalS(a, b, c, d, v, xg[j])
-        plt.plot(v,torque, "o")
+        # plt.plot(v,torque, "o")
         plt.plot(xg,yg, label = Wid[i])
-        plt.legend()
+        
+    if gearShift is not None:
+        plt.plot(gearShift[0], gearShift[1], "ko")
+        plt.annotate("Velocidade ideal", (gearShift[0][-1], gearShift[1][-1]), 
+                     xytext=(gearShift[0][-1]  + 10, gearShift[1][-1] - 110), 
+                     arrowprops=dict(facecolor='black', shrink=0.05, width=0.5, headwidth=4),
+                     bbox=dict(facecolor='lightgray', boxstyle = 'round, pad=0.3'))
     plt.xlabel("Velocidade (m/s)")
     plt.ylabel("Torque (Nm)")
+    plt.legend()
     plt.show()
+
+if __name__ == "__main__":
+    # Dados para Ferrari 360 Modena
+    # Raio
+    R = 0.3385 # metros
+
+    # Razão das marchas 
+    ratio = np.array([ 14.59,   9.58,   7.14,   5.63,   4.59,   3.77])
+
+    # Tabela de Dados velocidade de rotaçao (rad/s) vs torque (Nm)
+    w      = np.array([ 104.7,  157. ,  209.4,  261.8,  314.15,  366.5 ,  418.9 , 471.2 ,  523.6 ,  
+                       575.9 ,  628.3 ,  680.7 ,  733.  ,  785.4 , 837.7 ,  890.1 ,  942.5 ])
+    torque = np.array([ 170.8,  256.2,  298.8,  324.5,   341.5,  353.7,   362.9,  370. ,   372.8 ,  
+                       371.3 ,  368.3 ,  363.7 ,  357.6 ,  350.  ,  340.9,  330.3 ,  280.7 ]) 
+
+
+    plot(torque, w, ratio, R, gearShift(torque, w, ratio, R))
     
 
 
